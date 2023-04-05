37f77c50dafa1106774767a81baaa4acc66c2a86
Himanshu Srivastava <himanshusrivastava5390@gmail.com>
2023-04-06 00:36:20 +0530

    Added solution - LeetHub

---
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> printFirstNegativeInteger(long long int arr[],
+                                             long long int n, long long int k);
+
+// Driver program to test above functions
+int main() {
+    long long int t, i;
+    cin >> t;
+    while (t--) {
+        long long int n;
+        cin >> n;
+        long long int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        long long int k;
+        cin >> k;
+
+        vector<long long> ans = printFirstNegativeInteger(arr, n, k);
+        for (auto it : ans) cout << it << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+vector<long long> printFirstNegativeInteger(long long int A[],
+                                             long long int N, long long int K) {
+                                                 
+           deque<long long int>dq;
+        vector<long long> ans;
+        
+        // process first window of k size
+        for(int i = 0;i<K;i++){
+            if(A[i] < 0){
+                dq.push_back(i);
+            }
+        }
+        
+        // store ans of first k size window
+        if(dq.size() > 0){
+            ans.push_back(A[dq.front()]);
+        }else{
+            ans.push_back(0);
+        }
+        
+        // prcess for remaining  window
+        for(int i = K;i<N;i++){
+            // removal
+            if(!dq.empty() && i - dq.front() >= K){
+                dq.pop_front();
+            }
+            
+            // adition
+            if(A[i] < 0){
+                dq.push_back(i);
+            }
+            
+            // ans store
+            
+            if(dq.size() > 0){
+                ans.push_back(A[dq.front()]);
+            }else{
+                ans.push_back(0);
+            }
+        }
+        return ans;
+ }
