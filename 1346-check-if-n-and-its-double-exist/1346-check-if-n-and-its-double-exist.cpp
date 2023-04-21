5da9da2272b04a1d0d2313eb6a8b17f0dd7e1e9f
Himanshu Srivastava <himanshusrivastava5390@gmail.com>
2023-04-22 00:18:02 +0530

    Time: 3 ms (96.63%), Space: 9.9 MB (96.11%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        
+        int i = -1;
+        int j = -1;
+        
+        for (i = 0; i < arr.size(); i++) {
+            int n = arr[i];
+            for (j = i + 1; j < arr.size(); j++) {
+                int m = arr[j];
+                if ( n == (2 * m) || m == (2 * n)) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+        
+    }
+};
