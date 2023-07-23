d1c9033a0c9662389e69964ac9806e8b015d5af5
Himanshu Srivastava <himanshusrivastava5390@gmail.com>
2023-07-23 19:18:41 +0530

    Time: 8 ms (31.95%), Space: 12.1 MB (19.94%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,39 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        
+        // APPROACH 1 : USING STL
+        // next_permutation(nums.begin(),nums.end());
+        
+        // APPROACH 2
+        
+        int breakPoint = -1;
+        
+        // Step 1: find break point
+        for(int i = nums.size()-1; i>0; i--){
+            if(nums[i] > nums[i-1]){
+                breakPoint = i-1;
+                break;
+            }
+        }
+        
+        // step 2: if no break point
+        if(breakPoint < 0){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        
+        // step 3: if breakpoint exist
+        for(int i = nums.size()-1;  i>0; i--){
+            if(nums[i] > nums[breakPoint]){
+                swap(nums[i],nums[breakPoint]);
+                reverse(nums.begin() + breakPoint +1 , nums.end());
+                break;
+            }
+            
+        }
+        
+        
        
     }
 };
