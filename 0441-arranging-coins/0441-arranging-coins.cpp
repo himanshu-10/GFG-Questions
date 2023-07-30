8621d59c917f8b3cbaa8bfb14d58fb1e6c0edf35
Himanshu Srivastava <himanshusrivastava5390@gmail.com>
2023-07-30 12:56:24 +0530

    Time: 10 ms (36.91%), Space: 5.9 MB (39.83%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        
+        int row =1;
+        while(n>0){
+            row++;
+            n= n-row;
+        }
+        return row -1;
+    }
+};
