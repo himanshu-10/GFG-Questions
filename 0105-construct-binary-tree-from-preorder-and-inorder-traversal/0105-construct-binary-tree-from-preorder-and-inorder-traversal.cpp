4ce1448ce640d290d3d70109063d3fbbb1d533be
Himanshu Srivastava <himanshusrivastava5390@gmail.com>
2023-08-26 18:19:10 +0530

    Time: 60 ms (9.95%), Space: 269.8 MB (5.35%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int findPos(vector<int>inorder,int element){
+        for(int  i = 0;i<inorder.size();i++){
+            if(inorder[i] == element){
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    TreeNode* solve(vector<int> preorder, vector<int> inorder, int &index, int inorderStart,int inorderEnd){
+            
+        // base case
+        if(index > preorder.size() ){
+            return NULL;
+        }
+        
+        if(inorderStart > inorderEnd){
+            return NULL;
+        }
+        // pick starting element from preorder & increment index
+        int element = preorder[index++];
+        // make root
+        TreeNode* root = new TreeNode(element);
+        // find position of root in inorder
+        int position = findPos(inorder,element);
+        
+        // recursive call
+        root -> left = solve(preorder,inorder,index,inorderStart,position-1);
+        root -> right = solve(preorder,inorder,index,position+1,inorderEnd);
+        
+        return root;
+        
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+       
+        int preOrderIndex = 0;
+        // preorder - inorder - preorderindex - startIndexOfInorder - endIndexOfInorder
+        TreeNode*ans = solve(preorder, inorder, preOrderIndex, 0, inorder.size()-1);
+        return ans;
+    }
+};
